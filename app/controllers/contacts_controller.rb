4b486f6c32890de77e8654e0ea525c59ac5461e2
Rocco Stanzione <trappist@dev.collective-media.net>
2012-02-04 15:40:28 -0600

    Only show active users

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < ApplicationController
   
   def index
-    @users = User.all(:conditions => "type != 'AnonymousUser'")
+    @users = User.active.all(:conditions => "type != 'AnonymousUser'")
     @fields = UserCustomField.find(Setting.plugin_redmine_contacts[:contact_fields])
   end
 
